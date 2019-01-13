555550af9331e7985b7ffb48936610192c821231
Rashmi <baheti.rash123@gmail.com>
2019-01-13 13:55:51 -0500

    adds new react app called contacts
    - adds contacts list

---
+++ contacts/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ContactList extends React.Component {
+
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContactList contacts={[
+          { name: 'Tyler '},
+          { name: 'Karen' },
+          { name: 'Richard '}
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Mikenzi' },
+          { name: 'Ryan' }
+        ]}/>
+      </div>
+    );
+  }
+}
+
+export default App;
